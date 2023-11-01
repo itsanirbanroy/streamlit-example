d2befc57930041ad9733cec1abd3b8ac58adbb7c
itsanirbanroy <itsanirbanroy@gmail.com>
2023-11-01 08:05:58 +0000

    langchain update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,36 @@
 import streamlit as st
 from langchain.llms import OpenAI
-
-st.title('🦜🔗 Quickstart App')
+from langchain.document_loaders import PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
+st.title("📝 File Q&A ") 
+uploaded_file = st.file_uploader("Upload an article", type=("txt", "md")) 
+
+
+if uploaded_file and question and not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+
+if uploaded_file and question and openai_api_key:
+    loader = PyPDFLoader(uploaded_file)
+    pages = loader.load_and_split()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+    index = VectorstoreIndexCreator(embeddings_model).from_loaders([loader])
+    st.info(index.query(input_text))
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  question = st.text_input(
+    "Ask something about the article",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+  )
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+    generate_response(question)
