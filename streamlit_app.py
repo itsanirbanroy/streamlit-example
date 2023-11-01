dc147334c605cf275bd8225708755fd8530369fc
itsanirbanroy <itsanirbanroy@gmail.com>
2023-11-01 10:59:08 +0000

    Updated script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.document_loaders import PyPDFLoader
@@ -7,22 +8,37 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
+embeddings_model = None
+index = None
 
 st.title("📝 File Q&A ") 
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md", "pdf")) 
-tmp_location = os.path.join('/tmp', uploaded_file.filename)
+
+#tmp_location = os.path.join('/tmp', uploaded_file.filename)
 
 
 if uploaded_file and not openai_api_key:
     st.info("Please add your OpenAI API key to continue.")
 
-if uploaded_file and openai_api_key:
-    loader = PyPDFLoader(tmp_location)
+
+def createEmbedding():
+   if uploaded_file and openai_api_key:
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = PyPDFLoader(temp_filepath)
     pages = loader.load_and_split()
+
+    global index
     embeddings_model = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    index = VectorstoreIndexCreator(embedding=embeddings_model).from_loaders([loader])
 
 def generate_response(input_text):
-    index = VectorstoreIndexCreator(embeddings_model).from_loaders([loader])
+    global index
+    if(index is None):
+      createEmbedding()
+    #index = VectorstoreIndexCreator(embeddings_model).from_loaders([loader])
     st.info(index.query(input_text))
 
 with st.form('my_form'):
